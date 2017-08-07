570ce002faa25e5706e3b9b8fc3d969f7c881509
trungdovan <trungdovan87@gmail.com>
2017-08-07 15:54:19 +0700

    delete duplicate file

--- tdv/tdv-service/src/main/java/com/oddle/tdv/storage/configuration/StorageConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.oddle.tdv.storage.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("config-storage.properties")
-public class StorageConfiguration {
-}
