6f9ee124a1da96196280cfd439f33ebdf0539b6e
trungdovan <trungdovan87@gmail.com>
2017-08-08 18:49:26 +0700

    Done: Api - get

--- tdv/tdv-service/src/main/java/com/oddle/tdv/service/business/impl/ApiServiceImpl.java
+++ tdv/tdv-service/src/main/java/com/oddle/tdv/service/business/impl/ApiServiceImpl.java
@@ -1,14 +1,21 @@
 package com.oddle.tdv.service.business.impl;
 
+import com.oddle.tdv.db.sql.model.City;
 import com.oddle.tdv.db.sql.model.Weather;
 import com.oddle.tdv.service.business.ApiService;
+import com.oddle.tdv.storage.db.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class ApiServiceImpl implements ApiService{
+    @Autowired
+    private CityRepository cityRepo;
+
     @Override
     public Optional<Weather> get(long id) {
        // return Optional.of(new Weather());
-        return Optional.empty();
+        Optional<City> optional = cityRepo.findById(id);
+        return (optional.isPresent() && optional.get().getWeather() != null) ? Optional.of(optional.get().getWeather()) : Optional.empty();
     }
 }
