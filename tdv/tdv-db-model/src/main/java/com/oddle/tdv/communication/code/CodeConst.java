9f9444509be1c828f4e8f228c4db84e2109b1bf4
trungdovan <trungdovan87@gmail.com>
2017-08-08 15:50:31 +0700

    Done - Weather - Save

--- tdv/tdv-db-model/src/main/java/com/oddle/tdv/communication/code/CodeConst.java
+++ tdv/tdv-db-model/src/main/java/com/oddle/tdv/communication/code/CodeConst.java
@@ -9,6 +9,9 @@ public class CodeConst {
     public static int CODE_CITY_NAME_EXIST = 1011;
     public static String MSG_CITY_NAME_EXIST = "City with name = '%s' exist";
 
-    public static int CODE_CITY_ID_IS_NULL = 1010;
+    public static int CODE_CITY_ID_IS_NULL = 1012;
     public static String MSG_CITY_ID_IS_NULL = "City ID must not be NULL";
+
+    public static int CODE_CITY_ID_IS_NOT_EXIST = 1013;
+    public static String MSG_CITY_ID_IS_NOT_EXIST = "City with ID = '%d' must be exist";
 }
--- tdv/tdv-service/src/main/java/com/oddle/tdv/service/filter/WeatherStorageValidator.java
+++ tdv/tdv-service/src/main/java/com/oddle/tdv/service/filter/WeatherStorageValidator.java
@@ -1,9 +1,31 @@
 package com.oddle.tdv.service.filter;
 
+import com.oddle.tdv.communication.base.EResponseCode;
+import com.oddle.tdv.communication.base.SubCode;
+import com.oddle.tdv.communication.code.CodeConst;
 import com.oddle.tdv.communication.request.WeatherRequest;
+import com.oddle.tdv.db.sql.model.City;
+import com.oddle.tdv.exception.OddleException;
+import com.oddle.tdv.storage.db.CityRepository;
+import org.hibernate.LazyInitializationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class WeatherStorageValidator {
+    @Autowired
+    private CityRepository cityRepo;
+
     public boolean validateSave(WeatherRequest request) {
+        try {
+            Optional<City> optional =cityRepo.findOne(request.getCityId());
+            cityRepo.getOne(request.getCityId()).getId();
+        } catch (LazyInitializationException ex) {
+            throw new OddleException(EResponseCode.WRONG_DATA,
+                    new SubCode(CodeConst.CODE_CITY_ID_IS_NULL,
+                            String.format(CodeConst.MSG_CITY_ID_IS_NOT_EXIST, request.getCityId())
+                    ));
+        }
         return true;
     }
 }
