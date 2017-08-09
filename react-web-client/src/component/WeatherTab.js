27108484383b30789e32e42557d890bf8cb42b92
trungdovan <trungdovan87@gmail.com>
2017-08-09 13:59:02 +0700

    add Edit button

--- react-web-client/src/component/WeatherTab.js
+++ react-web-client/src/component/WeatherTab.js
@@ -1,13 +1,79 @@
 import React, {Component} from "react";
+import api from "../api/service/WeatherApi";
 
 class WeatherTab extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+
+        };
+        // this.onTextChange = this.onTextChange.bind(this);
+    }
+
+    onTextChange = (event) => {
+        let that = this;
+        api.searchWeather(event.target.value).then(
+            json => {
+                if (json.status === 0) {
+                    that.setState({cities: json.data})
+                } else {
+                    alert("Error: " + json);
+                }
+            }
+        );
+    };
+
+    componentDidMount() {
+        this.nameInput.focus();
+    }
+
+    _convertCityToLI(city) {
+
+        return (
+            <li key={city.cityId}>
+                <b> {city.cityName} </b> ({city.createDate})
+
+                <br/>
+                temp: {city.temperature} degree | {city.status} | {city.windy} m/s | {city.humidity}% | {city.pressure} hpa | --
+
+                <button> Edit </button>
+            </li>
+        )
+    }
+
+    _result() {
+        if (this.state.cities === undefined) {
+            return <div> Type city name to search </div>;
+        }  else if (this.state.cities.length === 0) {
+            return <div> No result </div>
+        } else {
+            return (
+                <ul>
+                    {this.state.cities.map(this._convertCityToLI)}
+                </ul>
+            )
+        }
+    }
+
 
     render() {
         return (
             <div>
-                <b> Weather Tab </b>
+                <p> <b> Weather Tab </b> </p>
+                <input type="text" onChange={this.onTextChange} ref={(input) => {
+                    this.nameInput = input;
+                }}/>
+                <p/>
+                {this._result()}
+                <p>---------------</p>
+                <a href=""
+                   onClick={(e) => { e.preventDefault(); this.props.selectScreen("home")}}
+                >
+                    HOME
+                </a>
             </div>
-        )
+        );
     }
 }
 export default WeatherTab
